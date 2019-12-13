a2fa8f4453d058f481f6e39023aa9254d0d64814
Alexander Biryulyov <medvedkama@icloud.com>
2019-12-13 18:16:44 +0300

    Update base code from reflection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
-    //private static final String CLASSFORTEST = "java.lang.String";
-    private static final String CLASSFORTEST = "java.lang.reflect.Method";
+    private static final String CLASSFORTEST = "java.lang.String";
+    //private static final String CLASSFORTEST = "java.lang.reflect.Method";
 
     public static void main(String[] args) {
         Information info = new Information();
         try {
             Class clazz = Class.forName(CLASSFORTEST);
             info.getInfo(clazz);
-            System.out.println(Information.sb);
+            info.printClass();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
