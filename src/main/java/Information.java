330d2407225fef0286dcc24e59680cc0fba83f23
Alexander Biryulyov <medvedkama@icloud.com>
2019-12-13 14:20:24 +0300

    first commit

---
+++ src/main/java/Information.java
@@ -0,0 +1,51 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Information {
+
+    public static StringBuilder sb = new StringBuilder();
+
+    public void getInfo(Class clazz) {
+        getFields(clazz);
+        getMethods(clazz);
+        getInterfaces(clazz);
+        getParent(clazz);
+    }
+
+    private void getFields(Class clazz) {
+        String Fields = null;
+        Field[] field = clazz.getDeclaredFields();
+        int cout = field.length;
+        sb.append("\nFields: \n" + "==========================" + "\t ").append(cout).append("\n");
+        for (int i = 0; i < field.length; i++) {
+         sb.append(field[i]).append("\n");
+        }
+    }
+
+    private void getInterfaces(Class clazz) {
+        String Fields = null;
+        Class[] field = clazz.getInterfaces();
+        int cout = field.length;
+        sb.append("\nInterfaces: \n" + "==========================" + "\t ").append(cout).append("\n");
+        for (int i = 0; i < field.length; i++) {
+            sb.append(field[i]).append("\n");
+        }
+    }
+
+    private void getMethods(Class clazz) {
+        String Methods = null;
+        Method[] methods = clazz.getDeclaredMethods();
+        int cout = methods.length;
+        sb.append("\nMethods: \n" + "==========================" + "\t").append(cout).append("\n");
+        for (int i = 0; i < methods.length; i++) {
+            sb.append(methods[i]).append("\n");
+        }
+    }
+
+    private void getParent(Class clazz) {
+        if (clazz.getSuperclass() != null) {
+            sb.append("\nSuperclass: \n" + "========================== \n" + clazz.getSuperclass().getName()).append("\n");
+            getInfo(clazz.getSuperclass());
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    //private static final String CLASSFORTEST = "java.lang.String";
+    private static final String CLASSFORTEST = "java.lang.reflect.Method";
+
+    public static void main(String[] args) {
+        Information info = new Information();
+        try {
+            Class clazz = Class.forName(CLASSFORTEST);
+            info.getInfo(clazz);
+            System.out.println(Information.sb);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
